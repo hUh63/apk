00929c76d16c39f134e9919f6b0d0ccbaf698b11
hUh63 <3133565923@qq.com>
2026-08-20 19:43:46 +0800

    fix: 修复 desktop backup_management.dart 的 FilePicker API (#159)

--- lib/ui/desktop/setting/backup_management.dart
+++ lib/ui/desktop/setting/backup_management.dart
@@ -357,7 +357,7 @@ class _DesktopBackupManagementState extends State<DesktopBackupManagement> {
   }
 
   Future<void> _exportBackup(BuildContext context, _BackupFile backup) async {
-    final result = await FilePicker.platform.saveFile(
+    final result = await FilePicker().saveFile(
       dialogTitle: '导出备份文件',
       fileName: backup.name,
       type: FileType.custom,
